a1c03b4556e5d57d587c16cbcf3edebe7206bb8a
MennoScholten <54530840+MennoScholten@users.noreply.github.com>
2023-05-27 17:01:39 +0200

    Created the block class

--- ConcurrentTetris/Block.h
+++ ConcurrentTetris/Block.h
@@ -1,26 +1,60 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
-class Block
+class Block : public sf::Drawable
 {
 private:
-    int width = 30; // pixels
-    int height = 30; // pixels
-    int position_row = 0; // relative_position
-    int position_column = 0; // relative_position
+
+    sf::RectangleShape block;   // Datatype to draw a rectangular/square block 
+
+    const int width = 50; // Size in pixels 
+    const int height = 50; // Size in pixels
+
+    float originX = 0.0;    // X-axis Origin of the shape 
+    float originY = 0.0;    // Y-axis Origin of the shape
+
+    float positionX = 0.0;  // X-axis pixel position of the shape
+    float positionY = 0.0;  // Y-axis Pixel position of the shape
+
+    int position_row = 0; // position in gameboard          // Might be removed!
+    int position_column = 0; // position in gameboard       // Might be removed!
+
     sf::Color color = sf::Color::Transparent;
     bool isAlive = false;
 
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const; // Virtual function to draw custom classes 
+
 public:
     // Constructors
-    Block(int row, int col, const sf::Color& blockColor);
+    Block();    // Default constructor
+    Block(int row, int col, const sf::Color blockColor);   // Might be removed!
+    Block(float positionX, float positionY, float originX, float originY, sf::Color blockColor);
 
     // Accessors
     int getWidth() const;
     int getHeight() const;
     sf::Color getColor() const;
 
+    void getPosition(float& x, float& y);
+    float getPositionX();
+    float getPositionY();
+    void getOrigin(float& x, float& y);
+    float getOriginX();
+    float getOriginY();
+
     // Modifiers
-    void setPosition(int row, int col);
-    void setColor(const sf::Color& blockColor);
+
+    void setPosition(float x, float y);
+    void setPositionX(float x);
+    void setPositionY(float y);
+    void setOrigin(float x, float y);
+    void setOriginX(float x);
+    void setOriginY(float y);
+    void setColor(const sf::Color blockColor);
+
+    // Public Member functions
+    void moveBlock(float x, float y);   // Function to move block to (X-Y) pixel location
+    void rotateBlock(float a);          // Function to rotate block around origin by angle amount
+    sf::Transform getBlockTransform();
+
 };
